1ec327a9e85270042097674cb7ceb8462885a5d2
Sergey Zhenikhov <clicker@alga-corp.ru>
2024-09-19 17:39:33 +0500

    Пивел в порядок репозиторий

--- lib/screens/crm/crm_index_page/bloc/crm_index_page_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'crm_index_page_bloc.dart';
-
-sealed class CrmIndexPageEvent {}
-final class CrmIndexPageEventInital extends CrmIndexPageEvent {}
-final class CrmIndexPageEventListSpec extends CrmIndexPageEvent {}
-final class CrmIndexPageEventListRequestions extends CrmIndexPageEvent {}
--- lib/screens/crm/crm_requisition_pages/bloc/crm_requisition_pages_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'crm_requisition_pages_bloc.dart';
-
-
-abstract class CrmRequisitionPageEvent {}
-final class CrmRequisitionPageEventInital extends CrmRequisitionPageEvent {}
-final class CrmRequisitionPageEventLoadRequisitions extends CrmRequisitionPageEvent {}
